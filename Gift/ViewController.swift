8d999a6f5f4d464ee69264e54d7cf1129da7b377
Mink Alex Vina <64893437+anh-dz@users.noreply.github.com>
2023-08-23 22:08:11 +0700

    Initial commit

---
+++ Gift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gift
+//
+//  Created by Mink Alex Vina on 14/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
